2ff6f704cd602c4e2a5a85c0b382bfee2fcc2ffc
lovelixon <126704434+lovelixon@users.noreply.github.com>
2023-04-26 19:32:01 +0300

    Add files via upload

---
+++ index.html/my siteJS.js
@@ -0,0 +1,13 @@
+VANTA.CELLS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color1: 0x208c,
+  color2: 0x354bf2,
+  size: 0.80,
+  speed: 0.60
+})
